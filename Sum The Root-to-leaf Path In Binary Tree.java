f893e138c971e157b6bd94ed61a41a7f26ffbf12
Anitha Boppagowdanapura Shivalingaswamy <anithabs@users.noreply.github.com>
2018-01-22 21:14:14 -0600

    Update Sum The Root-to-leaf Path In Binary Tree.java

--- Sum The Root-to-leaf Path In Binary Tree.java
+++ Sum The Root-to-leaf Path In Binary Tree.java
@@ -1,1 +1,16 @@
 
+//Consider a binary tree in which each node contains a binary deigit.
+
+
+public static int sumRootToLeaf(BinaryTreeNode root){
+  return sumRootToLeafHealper(root, 0);
+}
+  
+public static int sumRootToLeafHealper(BinaryTreeNode root , int partialSum){
+  if(root == null){ return 0;}
+  partialSum = partialSum * 2 + root.data;
+  if( root.right == null && root.left == null){
+    return partialSum;
+  }
+  return sumRootToLeafHealper(root.left , partialSum) + sumRootToLeafHealper(root.right , partialSum);
+}
